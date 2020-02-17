ec623bb2f9dbbbf3a218fbf1bbb1442e567c97b1
olgahwang <khvanolga.v@gmail.com>
2020-02-16 17:47:11 -0800

    Build

--- src/sketch.js
+++ src/sketch.js
@@ -7,28 +7,31 @@ let windowWidth = 1200, windowHeight = 900;
 let shipX, shipY;
 let ship;
 
-function preload() {
-  cirTexture = loadImage('../assets/gold.png');
-  sqTexture = loadImage('../assets/metal.png');
-  trTexture = loadImage('../assets/glass.png');
-  bgImage = loadImage('../assets/background.jpg');
-  shipImage = loadImage('../assets/spaceship.png');
-  shipX = windowWidth*0.4;
-  shipY = windowHeight*0.7;
-}
+window.onload = function() {
+  function preload() {
+    cirTexture = loadImage('../assets/gold.png');
+    sqTexture = loadImage('../assets/metal.png');
+    trTexture = loadImage('../assets/glass.png');
+    bgImage = loadImage('../assets/background.jpg');
+    shipImage = loadImage('../assets/spaceship.png');
+    shipX = windowWidth*0.4;
+    shipY = windowHeight*0.7;
+  }
 
-function setup() {
-  createCanvas(windowWidth*0.9, windowHeight*0.9);
-  background(bgImage);
-  ship = new Ship(shipImage,shipX, shipY);
-}
+  function setup() {
+    createCanvas(windowWidth*0.9, windowHeight*0.9);
+    background(bgImage);
+    ship = new Ship(shipImage,shipX, shipY);
+  }
 
-function draw() {
-  clear();
-  background(bgImage);
-  ship.draw();
-}
+  function draw() {
+    clear();
+    background(bgImage);
+    ship.draw();
+  }
 
-function getRnd(min, max) {
-  return Math.floor(Math.random() * (max - min + 1) ) + min;
-}
+  function getRnd(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) ) + min;
+  }
+
+};
