88f099d680db4c1bc2d1f122256cad3f2f523d11
olgahwang <khvanolga.v@gmail.com>
2020-03-16 23:37:35 -0700

    Onboarding added

--- src/index.js
+++ src/index.js
@@ -1,23 +1,22 @@
+var currentScreen = document.getElementById("onboarding1");
 
-function plusDivs(n) {
-  showDivs(slideIndex += n);
+function showFirstPage() {
+  currentScreen = document.getElementById("onboarding1");
+  currentScreen.style.display = "flex";
+  var wrapperDiv = document.getElementById("onboardingWrapper");
+  wrapperDiv.style.display = "flex";
 }
 
-function showDivs(n) {
-  var i;
-  var x = document.getElementsByClassName("onboarding");
-  if (n > x.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = x.length} ;
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";
-  }
-  x[slideIndex-1].style.display = "flex";
+function showSecondPage() {
+  currentScreen.style.display = "none";
+  currentScreen = document.getElementById("onboarding2");
+  currentScreen.style.display = "flex";
+  var wrapperDiv = document.getElementById("onboardingWrapper");
+  wrapperDiv.style.display = "flex";
 }
 
 function startClicked(){
-  //var slideIndex = 1;
-  //showDivs(slideIndex);
   var oldBg = document.getElementById("mainMenuDiv");
-  console.log(oldBg);
   oldBg.style.display = "none";
+  showFirstPage();
 }
