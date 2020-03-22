13e2715aae8973f20e0120531fba15ed7a981822
olgahwang <khvanolga.v@gmail.com>
2020-03-21 20:50:22 -0700

    Keypressed added

--- src/index.js
+++ src/index.js
@@ -1,47 +1,52 @@
 var currentScreen = document.getElementById("onboarding1");
 var chosenLevel = 0;
+var cur = 0;
 
 function showFirstPage() {
   currentScreen = document.getElementById("onboarding1");
   currentScreen.style.display = "flex";
   var wrapperDiv = document.getElementById("onboardingWrapper");
   wrapperDiv.style.display = "flex";
+  cur++;
 }
 
 function showSecondPage() {
   currentScreen.style.display = "none";
   currentScreen = document.getElementById("onboarding2");
   currentScreen.style.display = "flex";
+  cur++;
 }
 
 function showThirdPage() {
   currentScreen.style.display = "none";
   currentScreen = document.getElementById("onboarding3");
   currentScreen.style.display = "flex";
+  cur++;
 }
 
-/*function startClicked(){
+function startClicked(){
   var oldBg = document.getElementById("mainMenuDiv");
   oldBg.style.display = "none";
   showFirstPage();
-}*/
+}
 
 function pizzaChosen(){
-  chosenLevel = 0;
-  var oldBg = document.getElementById("mainMenuDiv");
-  oldBg.style.display = "none";
-  showFirstPage();
+  chosenLevel = 1;
+  var pizza = document.getElementById("pizzaLevel");
+  pizza.style.boxShadow = "0px 0px 0px 4px #5FAD56";
+  var salad = document.getElementById("saladLevel");
+  salad.style.boxShadow = "0px 0px 0px 0px";
 }
 
 function saladChosen(){
-  chosenLevel = 1;
-  var oldBg = document.getElementById("mainMenuDiv");
-  oldBg.style.display = "none";
-  showFirstPage();
+  chosenLevel = 0;
+  var salad = document.getElementById("saladLevel");
+  salad.style.boxShadow = "0px 0px 0px 4px #5FAD56";
+  var pizza = document.getElementById("pizzaLevel");
+  pizza.style.boxShadow = "0px 0px 0px 0px";
 }
 
 function continueToGame(){
-  console.log("button clicked");
   if (chosenLevel == 0) {
     window.location.href = './pizza.html';
     console.log("Pizza");
@@ -53,3 +58,27 @@ function continueToGame(){
     }
   }
 }
+
+document.addEventListener('keypress', function(event) {
+    if(event.keyCode == 49) {
+      console.log("1 pressed");
+      if (chosenLevel == 0){
+        chosenLevel=1;
+        pizzaChosen();
+      } else if (chosenLevel == 1){
+        chosenLevel=0;
+        saladChosen();
+      }
+    }
+    if (event.keyCode == 51) {
+      if (cur == 0) {
+        startClicked();
+      } else if (cur == 1) {
+        showSecondPage();
+      } else if (cur == 2) {
+        showThirdPage();
+      } else if (cur == 3) {
+        continueToGame();
+      }
+    }
+});
